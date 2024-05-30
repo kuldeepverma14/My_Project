0dc2e7c2a13d51a8b0f7ab737c2dccb286cd953d
kuldeep verma <kuldeepvermaiitiant@gmail.com>
2024-05-30 06:26:45 +0530

    fdfd

--- src/Crud/GetData.js
+++ src/Crud/GetData.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
+
 
 const GetData = () => {
     const navigate = useNavigate()
@@ -8,6 +10,7 @@ const GetData = () => {
     const [updateData, setUpdateData] = useState(null)
     const [idUpdate, setId] = useState(null)
     const [edit, setEdit] = useState(false)
+    const { register, handleSubmit, formState: { errors } } = useForm()
     const handleUpdate = (id) => {
         setId(id)
         console.log(id)
@@ -15,12 +18,14 @@ const GetData = () => {
 
     const data = async () => {
         try {
+            // debugger;
             const res = await axios.get('https://dummyjson.com/products')
             console.log("sdaf", res)
             if (res?.status === 200) {
                 setMapData(res?.data?.products)
                 if (idUpdate !== null) {
                     const filteredData = res?.data?.products?.filter(item => item?.id === idUpdate)
+                    console.log(filteredData)
                     setUpdateData(filteredData)
                 }
             }
@@ -31,7 +36,7 @@ const GetData = () => {
     }
     useEffect(() => {
         data();
-    }, [])
+    }, [idUpdate])
 
     // UPDATE started
     console.log("updateData", updateData)
@@ -39,26 +44,33 @@ const GetData = () => {
     //     alert("dfg")
     //     navigate(`/update/${id}`)
     // }
-   
+
     const onClose = () => {
         setId(null)
+        setEdit(false)
     }
-    const update = async () => {
+    const onSubmit = async () => {
         try {
-            const res = await axios.put(`https://dummyjson.com/products/${idUpdate}`)
+            const updateRes = await axios.put(`https://dummyjson.com/products/${idUpdate}`)
+            console.log("upadted", updateRes)
+            if (updateRes.status === 200) {
+                data()
+                onClose()
+            }
         } catch (err) { console.log(err) }
     }
+    // console.log("dirt", edit)
     return (
         <div className="m-5 h-screen">
             <h1>This is Data Table</h1>
             <Link to="/create" > Add </Link>
             <table className="mt-10">
                 <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Tags</th>
-                    <th>Dimensions width</th>
-                    <th className="text-red-500">Action</th>
+                    <th className="px-2">ID</th>
+                    <th className="px-2">Title</th>
+                    <th className="px-2">Tags</th>
+                    <th className="px-2">Dimensions width</th>
+                    <th className="text-red-500 px-2">Action</th>
                 </tr>
                 {mapData?.map((item, i) => (
                     <tr key={i}>
@@ -69,7 +81,7 @@ const GetData = () => {
                         ))}
                         <td className="text-center">{item?.dimensions?.width}</td>
                         <td className="flex flex-col text-center ">
-                            <button onClick={() => handleUpdate(item?.id)}>Update</button>
+                            <button type="button" className="cursor-pointer" onClick={() => handleUpdate(item?.id)}>Update</button>
                             <button className="text-red-500">Delete</button>
                         </td>
                         {/* <td className="text-center">{item?.completed.toString().charAt(0).toUpperCase()}{item?.completed.toString().slice(1)}</td> */}
@@ -82,19 +94,23 @@ const GetData = () => {
                     <div className="shadow-lg p-4 bg-white  rounded-lg">
                         <button onClick={onClose} className="relative top-1 left-64 text-red-500">Close</button>
                         {!edit && updateData?.map((item, i) => (
-
                             <div className="flex gap-5" key={i}>
                                 <div>
                                     <input className="border border-black rounded-sm px-2" value={item.title} />
                                 </div>
-                                {/* <div>
-                                    <input className="border border-black rounded-sm px-2" value={item.tags} />
-                                </div> */}
+
                             </div>
-                        
-                        
                         ))}
-                            <button className="mt-5" onClick={()=>setEdit(true)}> Edit </button>
+                        {!edit ? <button className="mt-5" onClick={() => setEdit(true)}> Edit </button> :
+                            <div className="flex gap-5">
+                                <form onSubmit={handleSubmit(onSubmit)} >
+                                    <input className="border border-black"  {...register("title", { required: true })} />
+                                    {errors.title && <span> This is a required field </span>}
+                                </form>
+                                <button onClick={() => setEdit(false)}>Cancel</button>
+                                <button type="submit" onClick={onSubmit} >Update</button>
+                            </div>
+                        }
                     </div>
                 </div>
             )}

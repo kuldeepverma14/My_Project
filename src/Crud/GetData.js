da7e43a7516f9f825492c2b3b55d608783b2cfe3
kuldeep verma <kuldeepvermaiitiant@gmail.com>
2024-06-09 01:02:27 +0530

    dfdf

--- src/Crud/GetData.js
+++ src/Crud/GetData.js
@@ -8,14 +8,14 @@ const GetData = () => {
     const navigate = useNavigate()
     const [mapData, setMapData] = useState(null)
     const [updateData, setUpdateData] = useState(null)
-    const [idUpdate, setId] = useState(null)
+    const [idUpdate, setIdUpdate] = useState(null)
     const [edit, setEdit] = useState(false)
     const { register, handleSubmit, formState: { errors } } = useForm()
     const handleUpdate = (id) => {
-        setId(id)
+        setIdUpdate(id)
         console.log(id)
     };
-
+   
     const data = async () => {
         try {
             // debugger;
@@ -46,7 +46,7 @@ const GetData = () => {
     // }
 
     const onClose = () => {
-        setId(null)
+        setIdUpdate(null)
         setEdit(false)
     }
     const onSubmit = async () => {
@@ -60,6 +60,17 @@ const GetData = () => {
         } catch (err) { console.log(err) }
     }
     // console.log("dirt", edit)
+
+    const handleDelete = async(id) => {
+        console.log(id)
+        try{
+            const deleteRes = await axios.delete(`https://dummyjson.com/products/${id}`)
+            console.log(deleteRes)
+        }catch(err){
+            console.log(err)
+        }
+    };
+
     return (
         <div className="m-5 h-screen">
             <h1>This is Data Table</h1>
@@ -81,35 +92,31 @@ const GetData = () => {
                         ))}
                         <td className="text-center">{item?.dimensions?.width}</td>
                         <td className="flex flex-col text-center ">
-                            <button type="button" className="cursor-pointer" onClick={() => handleUpdate(item?.id)}>Update</button>
-                            <button className="text-red-500">Delete</button>
+                            <button type="button" onClick={() => handleUpdate(item?.id)}>Update</button>
+                            <button className="text-red-500" onClick={() => handleDelete(item?.id)}>Delete</button>
                         </td>
                         {/* <td className="text-center">{item?.completed.toString().charAt(0).toUpperCase()}{item?.completed.toString().slice(1)}</td> */}
                         {/* we have added toString( because i m getting item?.completed in boolean form not in string form) */}
                     </tr>))}
             </table>
-
             {idUpdate !== null && (
                 <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50  z-50">
-                    <div className="shadow-lg p-4 bg-white  rounded-lg">
-                        <button onClick={onClose} className="relative top-1 left-64 text-red-500">Close</button>
+                    <div className="shadow-lg p-4 bg-white flex flex-col rounded-lg">
+                        <button onClick={onClose} className="text-end text-red-500">Close</button>
                         {!edit && updateData?.map((item, i) => (
-                            <div className="flex gap-5" key={i}>
-                                <div>
-                                    <input className="border border-black rounded-sm px-2" value={item.title} />
-                                </div>
-
+                            <div className="mt-5 " key={i}>
+                                <input className="border border-black rounded-sm px-2" value={item.title} />
                             </div>
                         ))}
-                        {!edit ? <button className="mt-5" onClick={() => setEdit(true)}> Edit </button> :
-                            <div className="flex gap-5">
-                                <form onSubmit={handleSubmit(onSubmit)} >
-                                    <input className="border border-black"  {...register("title", { required: true })} />
-                                    {errors.title && <span> This is a required field </span>}
-                                </form>
-                                <button onClick={() => setEdit(false)}>Cancel</button>
-                                <button type="submit" onClick={onSubmit} >Update</button>
-                            </div>
+                        {!edit ? <button className="mt-5 text-start" onClick={() => setEdit(true)}> Edit </button> :
+                            <form onSubmit={handleSubmit(onSubmit)} >
+                                <input className="border border-black"  {...register("title", { required: true })} /> <br />
+                                {errors.title && <span className="text-red-500"> This is a required field </span>} <br />
+                                <div className="flex gap-5">
+                                    <button type="submit"  >Update</button>
+                                    <button onClick={() => setEdit(false)}>Cancel</button>
+                                </div>
+                            </form>
                         }
                     </div>
                 </div>
